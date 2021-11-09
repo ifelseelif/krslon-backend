1e1cfca3dccb7789f9b7a7559a910657fe34d4a7
Artem Kolokolov <yougrysha@gmail.com>
2021-11-09 06:51:42 +0300

    init repository

---
+++ src/main/java/com/ifelseelif/krslonbackend/KrslonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ifelseelif.krslonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrslonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrslonBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifelseelif/krslonbackend/KrslonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifelseelif.krslonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrslonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
